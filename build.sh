954d09dd3aa87cd04970ddf56a0198bb58541039
Parfait <parfaitndizihiwe@gmail.com>
2026-03-20 07:09:36 +0200

    create production superuser

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 python manage.py collectstatic --noinput
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'Admin1234!')" | python manage.py shell
