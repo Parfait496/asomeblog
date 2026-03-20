c608d247163cb7d297f68659e553679d478e51bf
Parfait <parfaitndizihiwe@gmail.com>
2026-03-20 09:25:43 +0200

    add article images and videos

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, ArticleImage, ArticleVideo
 
-admin.site.register(Article)
+class ArticleImageInline(admin.TabularInline):
+    model = ArticleImage
+    extra = 3  # shows 3 empty slots to upload
+
+class ArticleVideoInline(admin.TabularInline):
+    model = ArticleVideo
+    extra = 1
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ArticleImageInline, ArticleVideoInline]
+
+admin.site.register(Article, ArticleAdmin)
--- articles/models.py
+++ articles/models.py
@@ -1,23 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class Article(models.Model):
-   title = models.CharField(max_length=100)
-   slug = models.SlugField()
-   body = models.TextField()
-   date = models.DateTimeField(auto_now_add=True)
-   thumb = models.ImageField(default='default.png', blank=True)
-   author = models.ForeignKey(
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    thumb = models.ImageField(default='default.png', blank=True)
+    author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         null=True,
         blank=True
     )
 
-   def __str__(self):
-      return self.title
+    def __str__(self):
+        return self.title
 
-   def snippet(self):
-      return self.body[:50]+'...'
+    def snippet(self):
+        return self.body[:50]+'...'
+
+
+class ArticleImage(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='article_images/')
+    caption = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f"Image for {self.article.title}"
+
+
+class ArticleVideo(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='videos')
+    video = models.FileField(upload_to='article_videos/')
+    caption = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f"Video for {self.article.title}"
