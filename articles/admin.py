96e87cd13a142f7407bbbff6dbe477b670aed5b2
Parfait <parfaitndizihiwe@gmail.com>
2026-03-20 13:27:44 +0200

    fix admin inlines

--- articles/admin.py
+++ articles/admin.py
@@ -4,12 +4,16 @@ from .models import Article, ArticleImage, ArticleVideo
 class ArticleImageInline(admin.TabularInline):
     model = ArticleImage
     extra = 3  # shows 3 empty slots to upload
+    can_delete = True 
 
 class ArticleVideoInline(admin.TabularInline):
     model = ArticleVideo
     extra = 1
+    can_delete = True 
 
 class ArticleAdmin(admin.ModelAdmin):
     inlines = [ArticleImageInline, ArticleVideoInline]
+    list_display = ['title', 'author', 'date'] 
+   
 
 admin.site.register(Article, ArticleAdmin)
